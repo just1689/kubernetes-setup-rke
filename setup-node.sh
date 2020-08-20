aab17b59d43548d61370f66fc7ebee7146c9c050
Justin Tamblyn <just1689@gmail.com>
2020-08-20 19:46:58 +0200

    Update setup-node.sh

--- setup-node.sh
+++ setup-node.sh
@@ -1,6 +1,9 @@
 # Requirements
 # Ubuntu 18.04, network card on home network, OpenSSH installed during setup.
 
+sudo add-apt-repository ppa:apt-fast/stable -y
+sudo apt-get update
+sudo apt-get -y install apt-fast
 
 
 #
@@ -11,11 +14,11 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt remove docker docker-engine docker.io
-sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg -y
+sudo apt-fast install apt-transport-https ca-certificates curl software-properties-common gnupg -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install docker-ce -y
+sudo apt-fast install docker-ce -y
 
 sudo usermod -aG docker $USER
 exit
